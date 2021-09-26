d9465c2aaa36684bb151235cb0e60b9c59ddd1e7
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-09-26 09:41:49 +0600

    learned React+hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Provider } from "react-redux";
 import CakeContainer from "./components/CakeContainer";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 import store from "./redux/store";
 
 function App() {
 	return (
 		<Provider store={store}>
 			<div>
+				<HooksCakeContainer />
 				<CakeContainer />
 			</div>
 		</Provider>
