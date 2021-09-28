c15616d970410b9c993b5e63236c420e3f120400
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-09-28 06:54:15 +0600

    described payload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Provider } from "react-redux";
 import CakeContainer from "./components/CakeContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
 import HooksIcecreamContainer from "./components/HooksIcecreamContainer";
+import NewCakeContainer from "./components/NewCakeContainer";
 import store from "./redux/store";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 				<HooksCakeContainer />
 				<CakeContainer />
 				<HooksIcecreamContainer />
+				<NewCakeContainer />
 			</div>
 		</Provider>
 	);
